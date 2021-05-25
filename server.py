5600badea443c0169f1c2b94225717060a46e31d
Ramon Darwich <velociraptorbaiano@yahoo.com.br>
2021-05-25 11:15:34 -0300

    Added functionality + Added  [POST] '/recurso' path, to simulate resource management and lock / busy states + Added [GET] '/shutdown' path, to simulate an offline server

--- server.py
+++ server.py
@@ -1,10 +1,33 @@
 import json
+import threading
+import time
 from flask import Flask
 
 app = Flask(__name__)
+thr_lock = threading.Lock()
 
 
-@app.route('/info', methods=['GET'])
+@app.route('/shutdown', methods=['GET'])    # Call this to simulate a server shutdown
+def shtdwn():
+    pass
+
+
+@app.route('/recurso', methods=['POST'])    # Call this to simulate handling a resource
+def res():
+    return_code: int
+    thr = threading.Thread(target=thr_func, args=(thr_lock, ))
+    available = not thr_lock.locked()
+    if available:
+        thr_lock.acquire()
+        thr.start()
+        return_code = 200
+    else:
+        return_code = 409
+    server_res = {"disponivel": available}
+    return json.dumps(server_res), return_code
+
+
+@app.route('/info', methods=['GET'])        # Call this to pull info about this module
 def home():
     componente = "server"
     desc = "serve os clientes com servicos variados"
@@ -17,8 +40,15 @@ def home():
     return json.dumps(out)
 
 
+def thr_func(lock):
+    print("Thread fired, waiting 10s...")
+    time.sleep(10)
+    print("Thread ended")
+    lock.release()
+
+
 def main():
-    app.run(host='0.0.0.0', port=3004)
+    app.run(host='0.0.0.0', port=3002)
 
 
 if __name__ == "__main__":
