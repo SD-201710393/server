e88892fed2ca7f8938b28bff895dd12a0c82a0c4
Ramon Darwich <velociraptorbaiano@yahoo.com.br>
2021-05-27 11:51:04 -0300

    Heroku preparation 4 + Changed 'server' to use Heroku's defined port

--- server.py
+++ server.py
@@ -31,6 +31,7 @@ def res():
 
 @app.route('/info', methods=['GET'])        # Call this to pull info about this module
 def home():
+    print('Reached home')
     componente = "server"
     desc = "serve os clientes com servicos variados"
     ver = "0.1"
@@ -50,6 +51,7 @@ def thr_func(lock):
 
 
 def main():
+    print('Entry point reached')
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 3002)))
 
 
