eaecc822e7157e595b1db8b15f758a6d36a8bdff
Ramon Darwich <velociraptorbaiano@yahoo.com.br>
2021-06-02 12:10:58 -0300

    Debug functionality added + Added a debug POST method on '/info' for simulating states

--- server.py
+++ server.py
@@ -3,9 +3,28 @@ import os
 import threading
 import time
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
-thr_lock = threading.Lock()
+# Server details that get pulled from '/info' in their respective order
+componente = "server"
+ver = "0.3"
+desc = "serve os clientes com servicos variados"
+acess_point = "https://sd-rdm.herokuapp.com"
+is_busy = False                                # If true, it's busy or 'down'. Otherwise, it's 'up'
+uid = 2
+is_leader = False
+election = "valentao"
+
+
+@app.route('/eleicao', methods=['POST'])      # Call this to say this server is a leader
+def elected():
+    pass
+
+
+@app.route('/eleicao', methods=['GET'])      # Call this to acknowledge an election is in process
+def ack_election():
+    pass
 
 
 @app.route('/shutdown', methods=['GET'])    # Call this to simulate a server shutdown
@@ -15,37 +34,103 @@ def shtdwn():
 
 @app.route('/recurso', methods=['POST'])    # Call this to simulate handling a resource
 def res():
+    global is_busy
     return_code: int
-    thr = threading.Thread(target=thr_func, args=(thr_lock, ))
-    available = not thr_lock.locked()
-    if available:
-        thr_lock.acquire()
+    thr = threading.Thread(target=thr_func, args=())
+    if is_busy:
+        return_code = 409
+    else:
+        is_busy = True
         thr.start()
         return_code = 200
-    else:
-        return_code = 409
-    server_res = {"disponivel": available}
+    server_res = {"ocupado": is_busy}
     return json.dumps(server_res), return_code
 
 
+@app.route('/info', methods=['POST'])       # Call this to manually set info (DEBUG function)
+def d_set_info():
+    return_code: int
+    exp_amount = 8          # We expect the incoming JSON to have 8 keys
+    global componente
+    global ver
+    global desc
+    global acess_point
+    global is_busy
+    global uid
+    global is_leader
+    global election
+    # 400 - Bad Request (Data was invalid)
+    # 409 - Conflict
+    # 200 - OK
+    req = request.json
+
+    if len(req) < exp_amount:
+        return_code = 400
+        print("[DEBUG] Invalid length")
+    else:
+        try:
+            t_componente = req["componente"]
+            t_ver = req["versao"]
+            t_desc = req["descricao"]
+            t_acess_point = req["ponto_de_acesso"]
+            t_status = req["status"]
+            t_uid = req["identificacao"]
+            t_is_leader = req["lider"]
+            t_election = req["eleicao"]
+            # If we reached here, all entries were read successfully
+            componente = t_componente
+            ver = t_ver
+            desc = t_desc
+            acess_point = t_acess_point
+            is_busy = t_status
+            uid = t_uid
+            is_leader = t_is_leader
+            election = t_election
+            return_code = 200
+        except():           # If we reached here, something was wrong on the dictionary
+            return_code = 409
+            print("[DEBUG] One or more keys missing / invalid")
+
+    curr_server_details = {
+        "componente": componente,
+        "versao": ver,
+        "descricao": desc,
+        "ponto_de_acesso": acess_point,
+        "status": ("down" if is_busy else "up"),
+        "identificacao": uid,
+        "lider": int(is_leader),
+        "eleicao": election
+    }
+    out = {
+        "atualizado": (True if return_code == 200 else False),
+        "info_atual": curr_server_details
+
+    }
+    return out, return_code
+
+
 @app.route('/info', methods=['GET'])        # Call this to pull info about this module
-def home():
-    componente = "server"
-    desc = "serve os clientes com servicos variados"
-    ver = "0.1"
+def info():
     out = {
         "componente": componente,
+        "versao": ver,
         "descricao": desc,
-        "versao": ver
+        "ponto_de_acesso": acess_point,
+        "status": ("down" if is_busy else "up"),
+        "identificacao": uid,
+        "lider": int(is_leader),
+        "eleicao": election
     }
-    return json.dumps(out)
+
+    return json.dumps(out), 200
 
 
-def thr_func(lock):
-    print("Thread fired, waiting 10s...")
+def thr_func():
+    global is_busy
+    print("[DEBUG] Thread fired, waiting 10s...")
     time.sleep(10)
-    print("Thread ended")
-    lock.release()
+    print("[DEBUG] Thread ended")
+    is_busy = False
 
 
 def main():
